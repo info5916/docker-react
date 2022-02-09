fceb531fa4b2c4bd0c07265111acbb97cc4ebcc3
info5916 <info5916@gmail.com>
2022-02-09 21:09:40 +0200

    testing deployment 3

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
-import React from 'react'
-import ReactDOM from 'react-dom'
-
+import React from 'react';
 
 test('renders learn react link', () => {
   render(<App />);
